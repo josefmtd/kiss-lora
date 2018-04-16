fb66ffeddcac5856e017a802fca589e7fd0579cf
Josef Matondang <josstemat@gmail.com>
2018-04-16 19:39:41 +0700

    Added python wrapper and kiss init

---
+++ Bluetooth/ax25.py
@@ -0,0 +1,12 @@
+import sys
+import time
+import pexpect
+import subprocess
+import signal
+
+ax25 = pexpect.spawn("call -r -S 1 YD0SHY-2", echo = False)
+ax25.expect('Rawmode')
+message = 'Uji coba mengirimkan pesan menggunakan INA-Rad dengan protokol AX.25'
+ax25.sendline(message)
+time.sleep(10)
+ax25.kill(signal.SIGKILL)
